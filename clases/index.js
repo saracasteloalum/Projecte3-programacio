dd5c487826ea0ed0d9b75ceaaf5f5ee5b332a8e7
jordimedinaalum-eng <jordimedina.alum@insestatut.cat>
2026-03-19 15:11:26 +0100

    Implementació clase TerminalUtils

--- clases/index.js
+++ clases/index.js
@@ -2,10 +2,12 @@ const {PaladiHuma} = require("./PaladiHuma");
 const {MagElf} = require("./MagElf");
 const {GuerrerNan} = require("./GuerrerNan");
 const {ArquerMitja} = require("./ArquerMitja");
+const {TerminalUtils} = require("./TerminalUtils");
 
 module.exports = {
     PaladiHuma,
     MagElf,
     GuerrerNan,
-    ArquerMitja
+    ArquerMitja,
+    TerminalUtils
 }
